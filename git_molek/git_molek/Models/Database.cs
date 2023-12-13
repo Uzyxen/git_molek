d3cac10ebba17b874be83b6c668dc02350062e75
GIZLO3 <michalgiza8b@gmail.com>
2023-12-13 10:17:20 +0100

    made Database model and method to insert DayMood

---
+++ git_molek/git_molek/Models/Database.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_molek.Models
+{
+    public class Database
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public Database(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<DayMood>();
+        }
+
+        public Task<int> InsertDayMoodAsync(DayMood dayMood)
+        {
+            return _database.InsertAsync(dayMood);
+        }
+    }
+}
