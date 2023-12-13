54110b4674e20f773e180261a5e3eb0fc5c31535
GIZLO3 <michalgiza8b@gmail.com>
2023-12-13 10:27:08 +0100

    added values to enum

--- git_molek/git_molek/Models/MoodEnum.cs
+++ git_molek/git_molek/Models/MoodEnum.cs
@@ -6,6 +6,10 @@ namespace git_molek.Models
 {
     public enum MoodEnum
     {
-
+        zadowolony = 0,
+        dobry = 1,
+        przeciętny = 2,
+        słaby = 3,
+        okropny = 4
     }
 }
