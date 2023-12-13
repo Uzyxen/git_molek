ba948ac7410fb736ef48f10d38d38c35fc03e081
GIZLO3 <michalgiza8b@gmail.com>
2023-12-13 10:40:30 +0100

    made mood buttons in code behind

--- git_molek/git_molek/MainPage.xaml.cs
+++ git_molek/git_molek/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using git_molek.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -10,9 +11,49 @@ namespace git_molek
 {
     public partial class MainPage : ContentPage
     {
+        public static Dictionary<MoodEnum, string> DayMoodIcons = new Dictionary<MoodEnum, string>()
+        {
+            { MoodEnum.zadowolony, "good.png"},
+            { MoodEnum.dobry, "mid.png"},
+            { MoodEnum.przeciętny, "meh.png"},
+            { MoodEnum.słaby, "sad.png"},
+            { MoodEnum.okropny, "bad.png"}
+        };
+
+        private static List<ImageButton> dayMoodImageButtons = new List<ImageButton>()
+        {
+            new ImageButton()
+            {
+                Source = DayMoodIcons[MoodEnum.zadowolony]
+            },
+            new ImageButton()
+            {
+                Source = DayMoodIcons[MoodEnum.dobry]
+            },
+            new ImageButton()
+            {
+                Source = DayMoodIcons[MoodEnum.przeciętny]
+            },
+            new ImageButton()
+            {
+                Source = DayMoodIcons[MoodEnum.słaby]
+            },
+            new ImageButton()
+            {
+                Source = DayMoodIcons[MoodEnum.okropny]
+            },
+        };
+
         public MainPage()
         {
             InitializeComponent();
+
+            for(int i=0; i < dayMoodImageButtons.Count; i++)
+            {
+                dayMoodImageButtons[i].Padding = 4;
+                buttonsGrid.Children.Add(dayMoodImageButtons[i]);
+                Grid.SetColumn(dayMoodImageButtons[i], i);
+            }
         }
     }
 }
