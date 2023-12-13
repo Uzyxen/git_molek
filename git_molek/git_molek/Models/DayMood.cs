b875ace34a6f14ca0538456e365cc71680b630bf
GIZLO3 <michalgiza8b@gmail.com>
2023-12-13 10:14:25 +0100

    made DayMood class model

---
+++ git_molek/git_molek/Models/DayMood.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace git_molek.Models
+{
+    public class DayMood
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public MoodEnum Mood { get; set; }
+    }
+}
